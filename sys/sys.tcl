25d6590a47373db9e0ed9416e1fce1b61f0dc747
turri21 <154733583+turri21@users.noreply.github.com>
2024-06-04 05:54:21 +0300

    Update sys.tcl

--- sys/sys.tcl
+++ sys/sys.tcl
@@ -16,6 +16,8 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK1_50
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK2_50
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to FPGA_CLK3_50
 set_location_assignment PIN_V11 -to FPGA_CLK1_50
+
+#Senhor: The clocks should be like that, otherwise the sound will be noisy.
 set_location_assignment PIN_Y13 -to FPGA_CLK2_50
 set_location_assignment PIN_E11 -to FPGA_CLK3_50
 
